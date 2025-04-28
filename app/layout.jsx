15d5aaa6f242e3360457a76ec784705c8938cf97
PumpaCto <pumpa6827@gmail.com>
2025-04-28 12:27:30 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,16 @@
-import './globals.css';
+import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export const metadata = {
   title: 'Miraverse Token',
-  description: 'Anime x Crypto x NFT x Game universe!',
+  description: 'The next big universe combining anime, NFTs, crypto, and open-world gaming!',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-black text-white">
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />

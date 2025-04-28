7f12f3859bd8de8aab07126614192893eebe01a3
PumpaCto <pumpa6827@gmail.com>
2025-04-28 12:19:27 +0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import './globals.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export const metadata = {
+  title: 'Miraverse Token',
+  description: 'Anime x Crypto x NFT x Game universe!',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
